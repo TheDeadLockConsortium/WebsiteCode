9dda920dfd78cc1ef1c625f146511a870f796e77
Trenton Chramosta <35844403+chramostatm@users.noreply.github.com>
2025-02-16 12:56:46 -0600

    images

--- code/strong-shell/partnerapp/partnerapp/models.py
+++ code/strong-shell/partnerapp/partnerapp/models.py
@@ -14,6 +14,6 @@ class Partner(models.Model):
     name= models.CharField(max_length=100)
     CompanySize= models.CharField(max_length=100,choices=SIZE_CHOICES)
     url= models.URLField(max_length=100)
-    #image=models.ImageField(upload_to='PartnerImages')
+    image=models.ImageField(upload_to='PartnerImages')
     def __str__(self):
         return self.name
