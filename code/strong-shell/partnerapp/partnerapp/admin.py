c0a504b7f851e5485ea09c02ba0cca2301efdc93
Trenton Chramosta <35844403+chramostatm@users.noreply.github.com>
2025-02-16 12:45:26 -0600

    typos

--- code/strong-shell/partnerapp/partnerapp/admin.py
+++ code/strong-shell/partnerapp/partnerapp/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import Partner
 
 class PartnerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'size', 'url')
+    list_display = ('name', 'url')
 
     search_fields =('name', 'url')
 
-    list_display_links = ('name')
+    list_display_links = ('name', 'url')
 
 admin.site.register(Partner, PartnerAdmin)
