abf3d383927ee794c288a4d8f9672c951fdd1723
Trenton Chramosta <35844403+chramostatm@users.noreply.github.com>
2025-02-18 16:41:01 -0600

    updated run-back.sh

--- code/strong-shell/run-back.sh
+++ code/strong-shell/run-back.sh
@@ -1,4 +1,5 @@
 python ./partnerapp/manage.py createsuperuser
+python ./partnerapp/manage.py makemigrations partnerapp
 python ./partnerapp/manage.py makemigrations
 python ./partnerapp/manage.py migrate
 python ./partnerapp/manage.py runserver
